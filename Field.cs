9b7f89ce68b14e51d9da17c7973b07a3fcd8b6cc
vladimirruppel <vladimir.ruppel2004@mail.ru>
2024-07-04 17:01:27 +0700

    Создан класс Field - поле, содержащее массив людей (объектов Person)

---
+++ Field.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerPractice
+{
+    class Field
+    {
+        public Person[] Grid { get; }
+        public int ColumnsCount { get; }
+        public int RowsCount { get; }
+
+        public Field(int columnsCount, int rowsCount)
+        {
+            ColumnsCount = columnsCount;
+            RowsCount = rowsCount;
+            Grid = new Person[RowsCount * ColumnsCount];
+            InitializeField();
+        }
+
+        private void InitializeField()
+        {
+            for (int i = 0; i < RowsCount; i++)
+            {
+                for (int j = 0; j < ColumnsCount; j++)
+                {
+                    Grid[i * RowsCount + j] = new Person(i, j);
+                }
+            }
+        }
+    }
+}
