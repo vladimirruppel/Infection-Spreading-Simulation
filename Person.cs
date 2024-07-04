2ab0eb6fbb2a083935fe999c7293fb6cfcf01f39
vladimirruppel <vladimir.ruppel2004@mail.ru>
2024-07-04 16:58:49 +0700

    Создан класс Person (человек), написана простейшая логика

---
+++ Person.cs
@@ -0,0 +1,41 @@
+﻿namespace SummerPractice
+{
+    public enum HealthStatus { Healthy, Infected, Recovered, Dead }
+
+    public class Person
+    {
+        public HealthStatus Status { get; set; } = HealthStatus.Healthy;
+        public int X { get; }
+        public int Y { get; }
+
+        public Person(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Infect()
+        {
+            if (Status == HealthStatus.Healthy)
+            {
+                Status = HealthStatus.Infected;
+            }
+        }
+
+        public void Recover()
+        {
+            if (Status == HealthStatus.Infected)
+            {
+                Status = HealthStatus.Recovered;
+            }
+        }
+
+        public void Die()
+        {
+            if (Status == HealthStatus.Infected)
+            {
+                Status = HealthStatus.Dead;
+            }
+        }
+    }
+}
